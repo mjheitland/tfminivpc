74a8354c0020c1665199520f8f802b02824fe9ab
heitlm <heitlm@amazon.de>
2022-01-07 17:35:59 +0100

    initial commit

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   # source = "../"
-  source = "git::https://github.com/mjheitland/tfminivpc.git?ref=1.0.0"
+  source = "git::https://github.com/mjheitland/tfminivpc.git?ref=1.0.1"
 
   cidr_block = "10.0.0.0/16"
 
