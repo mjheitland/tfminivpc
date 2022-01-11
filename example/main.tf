7464ff63df36eeb60563bf224e19333237b26e94
heitlm <heitlm@amazon.de>
2022-01-11 13:47:11 +0100

    using new provider

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   # source = "../"
-  source = "git::https://github.com/mjheitland/tfminivpc.git?ref=1.0.1"
+  # source = "git::https://github.com/mjheitland/tfminivpc.git?ref=1.0.1"
+  source = "heitland-it.de/mjheitland/tfminivpc/aws"
 
   cidr_block = "10.0.0.0/16"
 
